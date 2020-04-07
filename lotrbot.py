b3b5a4c9cfb65ea28cb2c7c8f74621b1a09a385b
Jakob Weiß <jakob.l.weiss@gmx.de>
2020-04-07 09:35:56 +0200

    new questions, some refactoring, fixed scoreboard

--- lotrbot.py
+++ lotrbot.py
@@ -16,28 +16,48 @@ ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
 # ==========================> CONFIG <==================================
 marker = '*'
 
-insults = ["Stupid fat hobbit! ~Smeagol","Fool of a took! ~Gandalf","I would cut off your head {}... if it stood but a little higher from the ground. ~Éomer",
+insults = ["Stupid fat {}! ~Smeagol","Fool of a {}! ~Gandalf","I would cut off your head {}... if it stood but a little higher from the ground. ~Éomer",
 "Dotard! What is the house of {} but a thatched barn where brigands drink in the reek, and their brats roll on the floor among the dogs? ~Saruman",
-"Hey, Stinker! Don't go getting too far behind. ~Sam","Feanor gave up because of your stupidity"]
+"Hey, {}! Don't go getting too far behind. ~Sam","Feanor gave up because of your stupidity, {}!"]
 
-compliments = ["Well done, my dear hobbit!","{}, you should be counted amongst the wise of middleearth.","I could not have done it better myself!"]
+compliments = ["Well done, my dear {}!","{}, you should be counted amongst the wise of middleearth.","Very good {}, I could not have done it better myself!"]
 
 scoreboard = {}
+blocked = [] #temporarily blocked users (cannot issue commands)
 
-def format_questionString(user,num,question,answers):
-    # random color
-    color = discord.Color.from_rgb(random.randint(0,255),random.randint(0,255),random.randint(0,255))
-    embed = discord.Embed(color=color)
-    embed.set_author(name="{}'s {} trial in the Arts of Middle Earth trivia".format(user.display_name,ordinal(num)), icon_url=user.avatar_url)
-    embed.title = question
-    embed.set_footer(text="A discord bot written in Python by JaWs")
-    ans_str = ""
-    for i in range(0,len(answers)):
-        ans_str += "    {}) {}\n".format(i+1,answers[i])
-    embed.description = ans_str
+def createEmbed(title,author_name,avatar_url,content,color,footnote):
+    embed = discord.Embed(color=discord.Color.from_rgb(color[0],color[1],color[2]))
+    embed.set_author(name=author_name, icon_url=avatar_url)
+    embed.title = title
+    embed.set_footer(text=footnote)
+    embed.description = content
     return embed
 
-def createMsg(user,insult=True):
+def createQuestion(user,num,question,answers):
+    # random color
+    color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+    author_name = "{}'s {} trial in the Arts of Middle Earth trivia".format(user.display_name,ordinal(num))
+    icon_url = user.avatar_url
+    title = question
+    footer = "A discord bot written in Python by JaWs"
+    content = ""
+    for i in range(0,len(answers)):
+        content += "    {}) {}\n".format(i+1,answers[i])
+
+    return createEmbed(title,author_name,icon_url,content,color,footer)
+
+def createScoreboard(user):
+    played,wins = scoreboard[str(user)]
+    ratio = wins/played
+    color = (int(255-ratio*255),int(ratio*255),0)
+    author_name = "{}'s results for their trials in the Art of Middle Earth trivia".format(user.display_name)
+    icon_url = user.avatar_url
+    title = "{}'s results".format(user.display_name)
+    content = "Trivia games played: {}\nTrivia games won: {}\nWin/Played ratio: {}%".format(played,wins,round(ratio*100,2))
+    footer = "A discord bot written in Python by JaWs"
+    return createEmbed(title,author_name,icon_url,content,color,footer)
+
+def createReply(user,insult=True):
     msg = insults[random.randint(0,len(insults)-1)] if insult else compliments[random.randint(0,len(compliments)-1)]
     return msg if "{}" not in msg else msg.format(user.display_name)
 
@@ -84,30 +104,37 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         user = message.author
+        name = str(user)
         content = message.content
         channel = message.channel
 
-        if user == client.user:
+        if user == client.user or user in blocked:
             return
 
         if content == "lotriv":
+
+            #get info from scoreboard
+            if name in scoreboard.keys():
+                print("user found in scoreboard!\n{}".format(scoreboard[name]))
+                count,wins = scoreboard[name]
+                count += 1
+            else:
+                print("user NOT found in scoreboard!")
+                count,wins = (1,0)
+            
             # get random question
             answers = self.questions[random.randint(0,len(self.questions)-1)].copy()
-            # strip the question (first element)
+            # pop the question (first element)
             question = answers.pop(0)
             # get the correct answer from the last element of the list
-            correct_answer = answers[int(answers[len(answers)-1])]
+            try:
+                correct_answer = answers[int(answers[len(answers)-1])]
+            except ValueError:
+                print("An error occured during retrieving the right index. Probably caused by a missing marker at the correct answer.\nThe question is: {}".format(question))
+                await channel.send("An error occured during parsing of the question. Please contact the developer at: `The_Legolas#1169`\nThe question was: `{}`".format(question))
+                return
             # remove the last element (the correct index)
             answers.pop()
-
-            # if the author is already in the scoreboard, retrieve info
-            if user in scoreboard.keys():
-                content = scoreboard[user]
-                scoreboard[user] = (content[0]+1,[content[1]])
-                count = content[0]+1
-            else:
-                scoreboard[user] = (1,0)
-                count = 1
 
             # shuffle answers
             random.shuffle(answers)            
@@ -116,28 +143,45 @@ class MyClient(discord.Client):
             ind = answers.index(correct_answer) + 1
 
             # send the question message
-            await message.channel.send(embed=format_questionString(user,count,question,answers))
+            await channel.send(embed=createQuestion(user,count,question,answers))
 
             def check(m):
                 return m.author == user and m.channel == channel
+
+            blocked.append(name)
             try:
                 msg = await client.wait_for('message',check=check,timeout=15)
             except asyncio.TimeoutError:
-                await message.channel.send(createMsg(user,insult=True)+"\nYou took to long to answer!")
+                await channel.send(createReply(user,insult=True)+"\nYou took too long to answer!")
                 return
+            blocked.remove(name)
 
-            if msg.content.isdigit(): # if msg is a digit
+            if msg.content.isdigit(): 
+                # if msg is a digit
                 if int(msg.content) == ind:
-                    await message.channel.send(createMsg(user,insult=False))
-                    scoreboard[user] = (scoreboard[user][0],scoreboard[user][1]+1) # increase the won games counter
+                    # right answer
+                    await channel.send(createReply(user,insult=False))
+                    wins += 1
                 else:
-                    await message.channel.send(createMsg(user,insult=True))
-            else: # not a digit
-                await message.channel.send(createMsg(user,insult=True)+"\nWhat is that supposed to be? Clearly not a digit...")
+                    # wrong answer
+                    await channel.send(createReply(user,insult=True))
+            else: 
+                # not a digit
+                await channel.send(createReply(user,insult=True)+"\nWhat is that supposed to be? Clearly not a digit...")
+            
+            scoreboard[name] = (count,wins)
+
+
+        elif content == "lotrscoreboard":
+            if name in scoreboard.keys():
+                await channel.send(embed=createScoreboard(user))
+            else:
+                await channel.send("You have to play a game of trivia before a scoreboard can be generated! use `lotriv` to take a quiz!")
                 
 try:
     with open("scoreboard.pyobj", 'rb') as sc_file:
         scoreboard = pickle.load(sc_file)
+        print("successfully loaded scoreboard.pyobj")
 except (FileNotFoundError,EOFError):
     print("scoreboard file not found, skipping.")
 
@@ -151,6 +195,6 @@ try:
         pickle.dump(scoreboard,sc_file)
 
 except (KeyboardInterrupt,RuntimeError):
-    print("\nShutting down...")
+    print("\nCatched error... Shutting down...")
     with open("scoreboard.pyobj", 'wb') as sc_file:
         pickle.dump(scoreboard,sc_file)
