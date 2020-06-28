97fba2c8a5d0114efabe7dd84148a6562e6f4da2
Jakob Weiß <jakob.l.weiss@gmx.de>
2020-06-28 21:28:22 +0200

    some script changes

--- script_parser.py
+++ script_parser.py
@@ -4,20 +4,26 @@ CONTINUATION_CHARS = ["’"]
 temp = ""
 last = ""
 cut_out = False
-import string
+
 with open("lotr_twotowers.txt", "r") as from_file:
     with open("lotr_twotowers_edited.txt", "w") as to_file:
         lines = from_file.readlines()
         for i, line in enumerate(lines):
-            temp = []
             line = line.strip()
+            if not line:
+                if i > 0 and i < len(lines)-1:
+                    if lines[i-1].strip() and lines[i+1].strip():
+                        to_file.write("\n")
+                        continue
             for char in line:
                 if char == "[":
                     cut_out = True
                 elif char == "]":
                     cut_out = False
-                
+                    continue
+
                 if not cut_out:
                     temp += char
-            to_file.write("\n")
-            
+            if not cut_out and temp.strip():
+                to_file.write(temp.strip()+"\n")
+                temp = ""
