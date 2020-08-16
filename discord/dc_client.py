f73445ac3773db7a70ea96240c4f887a41740c31
TheLegolas <jakob.l.weiss@gmx.de>
2020-08-16 09:26:58 +0200

    added superuser notification

--- discord/dc_client.py
+++ discord/dc_client.py
@@ -68,6 +68,8 @@ class LotrBot(discord.Client):
             if content[0] in self.config.DISCORD_CONFIG['settings.features']:
                 if channel.permissions_for(user).manage_channels or \
                    user.id in self.config.GENERAL_CONFIG['superusers']:
+                    if user.id in self.config.GENERAL_CONFIG['superusers']:
+                       await channel.send(":desktop: Superuser detected, overriding permissions...")
                     ret = minigames.edit_settings(content, self.settings, channel)
                     await channel.send(ret)
                 else:
