eacadf402ff677a9d0888924be656f9034931be1
Jakob Weiß <jakob.l.weiss@gmx.de>
2020-08-27 17:08:53 +0200

    added OS shaming function

--- discord/dc_client.py
+++ discord/dc_client.py
@@ -211,6 +211,14 @@ You are in a DM Channel... join a server where this amazing bot is present to cr
              minigames.feature_allowed('autoscript', channel, self.settings, self.config):
             result = minigames.find_similar_from_script\
             (message.content, self.script_condensed, self.script, self.config)
+            for trigger in self.config.DISCORD_CONFIG['windows.triggers']:
+                if trigger.lower() in message.split(' '):
+                    await channel.send(choice(self.config.DISCORD_CONFIG['windows.responses']))
+                    return
+            for trigger in self.config.DISCORD_CONFIG['apple.triggers']:
+                if trigger.lower() in message.split(' '):
+                    await channel.send(choice(self.config.DISCORD_CONFIG['apple.responses']))
+                    return
             if result:
                 try:
                     if channel.permissions_for(server.me).add_reactions:
