75a5f43e49d65fd010ec45332c01d5b2d39209ed
Jakob Weiß <jakob.l.weiss@gmx.de>
2020-08-27 17:10:02 +0200

    fix

--- discord/dc_client.py
+++ discord/dc_client.py
@@ -212,11 +212,11 @@ You are in a DM Channel... join a server where this amazing bot is present to cr
             result = minigames.find_similar_from_script\
             (message.content, self.script_condensed, self.script, self.config)
             for trigger in self.config.DISCORD_CONFIG['windows.triggers']:
-                if trigger.lower() in message.split(' '):
+                if trigger.lower() in message.content.lower.split(' '):
                     await channel.send(choice(self.config.DISCORD_CONFIG['windows.responses']))
                     return
             for trigger in self.config.DISCORD_CONFIG['apple.triggers']:
-                if trigger.lower() in message.split(' '):
+                if trigger.lower() in message.content.lower.split(' '):
                     await channel.send(choice(self.config.DISCORD_CONFIG['apple.responses']))
                     return
             if result:
