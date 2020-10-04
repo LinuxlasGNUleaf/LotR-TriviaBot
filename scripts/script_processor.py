04f9451c56067a2ed9a11d1df2b0fac5a95121cb
Jakob Weiß <jakob.l.weiss@gmx.de>
2020-10-04 15:13:41 +0200

    removing scene directions, updated script processor

--- scripts/script_processor.py
+++ scripts/script_processor.py
@@ -1,8 +1,27 @@
 empty = True
 actors = []
-with open('auj.txt', 'r') as from_file:
-    with open('auj_edited.txt', 'w') as to_file:
-        for line in from_file.readlines():
+with open('dos.txt', 'r') as from_file:
+    with open('dos_edited.txt', 'w') as to_file:
+        lines = from_file.readlines()
+        for ind, line in enumerate(lines):
+            line = line.strip()
+            if '[' in line:
+                start = line.index('[')
+                end = line.index(']')
+                if not(start >= 0 and end > start):
+                    print('Faulty line: {}\nstart:{}\nend:{}'.format(line, start, end))
+                    raise Exception("Error in line: {} invalid square brackets. Please remove.".format(ind+1))
+                if start == 0 and end == len(line)-1:
+                    continue
+                temp = line[start:end+1]
+                line = line.replace(temp, '').replace('  ', ' ').replace('  ', ' ')
+            to_file.write(line+'\n')
+
+exit(0)
+with open('dos.txt', 'r') as from_file:
+    with open('dos_edited.txt', 'w') as to_file:
+        lines = from_file.readlines()
+        for ind, line in enumerate(lines):
             if line.strip():
                 if empty:
                     line = line.upper()
