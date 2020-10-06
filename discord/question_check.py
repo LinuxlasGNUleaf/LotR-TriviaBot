8c63c0355e5a4f1cdfe22240d1bda25743f3f753
Jakob Weiß <jakob.l.weiss@gmx.de>
2020-10-06 21:29:44 +0200

    new questions from the Google Form, fixed old ones

---
+++ discord/question_check.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('questions.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for i, question in enumerate(csvreader):
+        if len(question) != 6:
+            print("Unusal number of arguments in this question: "+question[1])
+        for item in question:
+            if item.startswith('*'):
+                break
+        else:
+            print("Question without correct answer: "+question[1])
