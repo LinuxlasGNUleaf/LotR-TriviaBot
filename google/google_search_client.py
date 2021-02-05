e41654b4c7a631ec344d055fe8829759223bb5c1
Jakob Weiß <jakob.l.weiss@gmx.de>
2021-02-05 22:24:55 +0100

    final fix for google search

--- google/google_search_client.py
+++ google/google_search_client.py
@@ -5,6 +5,4 @@ class GoogleSearchClient:
         '''
         searches for a specific query
         '''
-        return search('{} {}'.format('site:'+site if site else '', query).strip(),
-                      tld='co.in', lang='en',
-                      num=10, stop=1, pause=1)
+        return search('{} {}'.format('site:'+site if site else '', query).strip(), 'co.in', 'en', stop=1, pause=1)
