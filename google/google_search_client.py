1a45a30b837dde4a486b94d3425d2626922e68f2
Jakob Weiß <jakob.l.weiss@gmx.de>
2021-02-05 22:38:54 +0100

    fixed possible namespace conflict

--- google/google_search_client.py
+++ google/google_search_client.py
@@ -1,8 +1,8 @@
-from googlesearch import search
+import googlesearch
 
 class GoogleSearchClient:
     def google_search(self, query, site):
         '''
         searches for a specific query
         '''
-        return search('{} {}'.format('site:'+site if site else '', query).strip(), 'co.in', 'en', stop=1, pause=1)
+        return googlesearch.search(('{} {}'.format('site:'+site if site else '', query).strip()), 'co.in', 'en', stop=1, pause=1)
