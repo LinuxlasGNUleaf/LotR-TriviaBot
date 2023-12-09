9f48246484dc1e499351fc82023bb18532011c6a
JaWs <jakob.l.weiss@gmx.de>
2023-12-09 23:59:55 +0100

    fix mistake in generate_questions_from_xlsx.py that marks correct answers incorrectly

--- tools/maintenance_scripts/generate_questions_from_xlsx.py
+++ tools/maintenance_scripts/generate_questions_from_xlsx.py
@@ -8,18 +8,21 @@ ws = load_workbook(data_file)['questions']
 # put values in matrix
 all_rows = [[str(cell.value) for cell in row] for row in ws.rows]
 # sort by source first and level second
-all_rows.sort(key= lambda row: row[1]+row[0].rjust(2))
+all_rows.sort(key=lambda row: row[1]+row[0].rjust(2))
 # add correct answer mark for questions where it is not marked
 for i, row in enumerate(all_rows):
     lvl, source, question, *answers = row
-    marked = False
-    for answer in answers:
+    correct_index = -1
+    for ind, answer in enumerate(answers):
         if answer.startswith('*'):
-            marked = True
+            correct_index = ind
             break
     # if no answer is marked, mark the first one
-    if not marked:
+    if correct_index == -1:
         answers[0] = f'*{answers[0]}'
+        correct_index = 0
+
+    correct_answer = answers.pop(correct_index)
     all_rows[i] = [source, lvl, question, *answers]
     print(all_rows[i])
 
