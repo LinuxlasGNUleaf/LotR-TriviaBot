bd5e0e590cf75c85d1c48c9a417ea246f505229f
JaWs <jakob.l.weiss@gmx.de>
2024-09-21 12:42:34 +0200

    HangmanCog.py skeleton

---
+++ cogs/HangmanCog.py
@@ -0,0 +1,113 @@
+import random
+import string
+
+import discord
+from discord import app_commands
+
+from src.DefaultCog import DefaultCog
+
+
+class HangmanCog(DefaultCog):
+    """
+    hangman game with ME-related characters and places
+    """
+
+    @app_commands.command(name='hangman')
+    async def hangman(self, interaction: discord.Interaction):
+        with open(self.assets['words'], 'r') as csvfile:
+            word = random.choice(csvfile.readlines()).strip()
+        word_condensed = word.lower().replace(' ', '')
+
+        steps = 2 if len(word_condensed) <= 6 else 1
+
+        used_chars = []  # used characters array
+        max_ind = len(game_states) - 1  # max index
+        ind = 0
+
+        h_embed = create_hangman_embed(
+            interaction.user, word, game_states[0], 0, [], True)
+        hangman = await ctx.send(embed=h_embed)
+
+        def check(chk_msg):
+            return chk_msg.author == ctx.message.author and chk_msg.channel == ctx.channel
+
+        while True:
+            try:
+                msg = await self.bot.wait_for('message', check=check,
+                                              timeout=self.options['timeout'])
+                msg = msg.content.lower()
+                for pattern in self.patterns:
+                    msg = re.sub(pattern, '', msg)
+
+            except asyncio.TimeoutError:
+                h_embed = create_hangman_embed(
+                    ctx.message.author, word, end_states[0], 8, used_chars, False)
+                await hangman.edit(embed=h_embed)
+                await ctx.send('Game over! You took too long to answer!')
+                break
+
+            for char in msg:
+                if char not in used_chars and char in string.ascii_lowercase:
+                    used_chars.append(char)
+                    if char not in word_condensed:
+                        ind += steps
+            used_chars.sort()
+
+            if ind > max_ind:
+                h_embed = create_hangman_embed(
+                    ctx.message.author, word, end_states[0], 8, used_chars, False)
+                await hangman.edit(embed=h_embed)
+                await ctx.send('Game over! You lost all your lives!')
+                break
+
+            for char in word_condensed:
+                if char not in used_chars and char.isalpha():
+                    break
+            else:
+                h_embed = create_hangman_embed(
+                    ctx.message.author, word, end_states[1], 0, used_chars, False)
+                await hangman.edit(embed=h_embed)
+                await ctx.send('Congratulations! You won the game!')
+                break
+
+            h_embed = create_hangman_embed(
+                ctx.author, word, game_states[ind], ind, used_chars, True)
+            await hangman.edit(embed=h_embed)
+
+
+    def create_hangman_embed(user: discord.Member, word, state, ind, used_chars, ongoing):
+        """
+        creates Hangman embed
+        """
+        hangman = ''
+        for char in word:
+            if char == ' ':
+                hangman += '  '
+                continue
+            if not char.isalpha():
+                hangman += char + ' '
+            elif char.lower() in used_chars or not ongoing:
+                hangman += '__{}__ '.format(char)
+            else:
+                hangman += r'\_ '
+        hangman = hangman.strip()
+
+        if used_chars:
+            used = 'Used letters:\n '
+            for char in used_chars:
+                used += ':regional_indicator_{}: '.format(char)
+            used = used.strip()
+        else:
+            used = ''
+
+        author_field = (
+        f'{user.display_name}\'s hangman game', None, (user.avatar if user.avatar else user.default_avatar).url)
+
+        color = discord.Color.from_rgb(int(bu.map_values(ind, 0, 8, 0, 255)),
+                                       int(bu.map_values(ind, 0, 8, 255, 0)),
+                                       0)
+
+        return du.create_embed(hangman, content=used + state, color=color, author_field=author_field)
+
+async def setup(bot):
+    await bot.add_cog(HangmanCog(bot))
